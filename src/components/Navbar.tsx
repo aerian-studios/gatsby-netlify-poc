ccafaf5dd0d44b5a0f8d6258693bd5427e3e9377
Sunya Dickman <sunya.dickman@aerian.com>
2018-03-02 17:52:54 +0000

    - changes to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,37 +5,41 @@ import github from "../img/github-icon.svg";
 import logo from "../img/logo.svg";
 
 const Navbar: React.SFC = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
-          </figure>
-        </Link>
+  <header id="page-header" className="block--full">
+    <nav className="navbar">
+      <div className="container">
+        <div className="navbar-brand">
+          <Link to="/" className="navbar-item">
+            <figure className="logo" style={{ width: "88px" }}>
+              <svg>
+                <use xlinkHref="#logo" />
+              </svg>
+            </figure>
+          </Link>
+        </div>
+        <div className="navbar-start">
+          <Link className="navbar-item" to="/about">
+            About
+          </Link>
+          <Link className="navbar-item" to="/products">
+            Products
+          </Link>
+        </div>
+        <div className="navbar-end">
+          <a
+            className="navbar-item"
+            href="https://github.com/aerian-studios"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <svg className="icon">
+              <use xlinkHref="#logo" />
+            </svg>
+          </a>
+        </div>
       </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/aerian-studios"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div>
-    </div>
-  </nav>
+    </nav>
+  </header>
 );
 
 export default Navbar;
