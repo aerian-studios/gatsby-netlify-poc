204cb00d52bfe81c96733c9b1b960ef880c8ece8
sunya <sunya.dickman@aerian.com>
2018-03-05 17:34:07 +0000

    - ditto

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,45 +1,63 @@
 import * as React from "react";
 import Link from "gatsby-link";
+import styled from "styled-components";
 
-import github from "../img/github-icon.svg";
-import logo from "../img/logo.svg";
+import * as github from "../img/github-icon.svg";
+import * as logo from "../img/logo.svg";
+
+const NavbarHeader = styled.header`
+  position: fixed;
+  width: 100%;
+  background-color: var(--c-black-90, #0c0c0c);
+  top: 0;
+  left: 0;
+  z-index: 300;
+  transition: transform 0.5s ease 0s, opacity 0.2s ease 0.2s;
+  height: 55px;
+  color: var(--whiteish, #fff);
+
+  & svg {
+    height: 55px;
+  }
+`;
+
+const LogoWrapper = styled.figure``;
 
 const Navbar: React.SFC = () => (
-  <header id="page-header" className="block--full">
-    <nav className="navbar">
-      <div className="container">
-        <div className="navbar-brand">
-          <Link to="/" className="navbar-item">
-            <figure className="logo" style={{ width: "88px" }}>
-              <svg>
-                <use xlinkHref="#logo" />
-              </svg>
-            </figure>
-          </Link>
-        </div>
-        <div className="navbar-start">
-          <Link className="navbar-item" to="/about">
-            About
-          </Link>
-          <Link className="navbar-item" to="/products">
-            Products
-          </Link>
-        </div>
-        <div className="navbar-end">
-          <a
-            className="navbar-item"
-            href="https://github.com/aerian-studios"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <svg className="icon">
-              <use xlinkHref="#logo" />
+  <NavbarHeader id="page-header" className="block--full">
+    <nav className="navbar layout-grid grid">
+      <div className="navbar-brand">
+        <Link to="/" className="navbar-item" title="Aerian Studios brand">
+          <figure className="logo" style={{ width: "88px" }}>
+            <svg>
+              <use xlinkHref={`#${logo.id}`} />
             </svg>
-          </a>
-        </div>
+          </figure>
+        </Link>
+      </div>
+      <div className="navbar-start">
+        <Link className="navbar-item" to="/about">
+          About
+        </Link>
+        <Link className="navbar-item" to="/products">
+          Products
+        </Link>
+      </div>
+      <div className="navbar-end">
+        <a
+          className="navbar-item"
+          href="https://github.com/aerian-studios"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="Visit Aerian studios' github account"
+        >
+          <svg className="icon">
+            <use xlinkHref={`#${github.id}`} />
+          </svg>
+        </a>
       </div>
     </nav>
-  </header>
+  </NavbarHeader>
 );
 
 export default Navbar;
