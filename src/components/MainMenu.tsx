fa448f628804933daa799a24d7ecb1e4020556ff
sunya <sunya.dickman@aerian.com>
2018-04-04 11:38:39 +0100

    - finished removing css modules, still need to apply the styles

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import Link from "gatsby-link";
 
-import * as styles from "./navBarStyles.module.scss";
+import * as styles from "./navBarStyles.scss";
 
 import * as github from "../img/github-icon.svg";
 
