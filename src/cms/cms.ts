b293cb7ab88ab4c21ee057ab6d88b023befe5992
Sunya Dickman <sunya.dickman@aerian.com>
2018-05-03 15:05:05 +0100

    with yarn and fix cms.ts

--- src/cms/cms.ts
+++ src/cms/cms.ts
@@ -1,5 +1,5 @@
 import * as React from "react";
-import * as CMS from "netlify-cms";
+import CMS from "netlify-cms";
 import "netlify-cms/dist/cms.css";
 
 import BlogPostPreview from "./preview-templates/BlogPostPreview";
