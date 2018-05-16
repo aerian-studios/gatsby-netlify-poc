87ccda3fcd339bc3f46df3412fa2a3b8b78f61b1
sunya <sunya.dickman@aerian.com>
2018-05-16 12:07:10 +0100

    adjusted the template for about

--- src/cms/preview-templates/AboutPagePreview.tsx
+++ src/cms/preview-templates/AboutPagePreview.tsx
@@ -1,10 +1,8 @@
 import * as React from "react";
-import Content, { HTMLContent } from "../../components/Content";
 import { AboutPageTemplate } from "../../templates/about-page";
 
 const AboutPagePreview = ({ entry, widgetFor }) => (
     <AboutPageTemplate
-        contentComponent={HTMLContent}
         title={entry.getIn(["data", "title"])}
         content={widgetFor("body")}
         heroImage={entry.getIn(["data", "heroimage"])}
