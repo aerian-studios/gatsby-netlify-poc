5ccfb12737367c5aba789e5fd89188131d742c93
sunya <sunya.dickman@aerian.com>
2018-05-16 09:33:53 +0100

    try out about page preview

--- src/cms/preview-templates/AboutPagePreview.tsx
+++ src/cms/preview-templates/AboutPagePreview.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
+import Content, { HTMLContent } from "../../components/Content";
 import { AboutPageTemplate } from "../../templates/about-page";
 
 const AboutPagePreview = ({ entry, widgetFor }) => (
-  <AboutPageTemplate
-    title={entry.getIn(["data", "title"])}
-    content={widgetFor("body")}
-  />
+    <AboutPageTemplate
+        contentComponent={HTMLContent}
+        title={entry.getIn(["data", "title"])}
+        content={widgetFor("body")}
+        heroImage={entry.getIn(["data", "heroimage"])}
+    />
 );
 
 export default AboutPagePreview;
