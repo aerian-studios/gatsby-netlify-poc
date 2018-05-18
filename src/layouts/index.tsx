588a47bf90545bace2e384f67c2ad538d88be646
sunya <sunya.dickman@aerian.com>
2018-05-18 11:42:32 +0100

    some meta-data fixes

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -7,18 +7,26 @@ import "../scss/base-theme.scss";
 import ErrorBoundary from "../components/ErrorBoundary";
 
 interface Props {
-  children: () => React.Component;
-  className?: string;
+    children: () => React.Component;
+    className?: string;
 }
 
 const TemplateWrapper: React.SFC<Props> = ({ children }) => (
-  <div className="layout-container">
-    <Helmet title="Home | Gatsby + Netlify CMS" />
-    <Navbar />
-    <ErrorBoundary>
-      <div id="content-wrapper">{children()}</div>
-    </ErrorBoundary>
-  </div>
+    <div className="layout-container">
+        <Helmet>
+            <html lang="en" />
+            <meta charSet="utf-8" />
+            <meta
+                name="description"
+                content="A little Proof of Concept for Gatsby and Netlify-CMS"
+            />
+            <title>Home | Gatsby + Netlify CMS"</title>
+        </Helmet>
+        <Navbar />
+        <ErrorBoundary>
+            <div id="content-wrapper">{children()}</div>
+        </ErrorBoundary>
+    </div>
 );
 
 export default TemplateWrapper;
